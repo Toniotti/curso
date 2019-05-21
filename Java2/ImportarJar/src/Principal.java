75cbb70aae74a6f4a3e198c2d18de3ac1c5007e3
Matheus <matheus.toniotti@gmail.com>
2019-05-21 11:40:03 -0300

    sla

--- Java2/ImportarJar/src/Principal.java
+++
@@ -1,8 +0,0 @@
-import pacote.Texto;
-
-public class Principal {
-	public static void main(String[] args) {
-		Texto t = new Texto("VAMO DALE");
-		t.contarLetras();
-	}
-}
--- bd/Write.java
+++ bd/Write.java
@@ -1,26 +1,37 @@
 package bd;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 
 public class Write {
 	
+	String fileName;
+	boolean append;
+	
 	Path path;
 	FileWriter writer;
 	
+	//create the file
 	public void createFile(String fileName, boolean append) {
-		
 		try {
+			this.fileName = fileName;
+			this.append = append;
 			writer = new FileWriter(fileName+".txt", append);
 			this.path = Path.of(fileName+".txt");
+			
 		} catch (Exception e) {
 		}
+		
 	}
 	
+	//insert text in file
 	//if the file is created by the "createFile" you don't need to specify a path
 	public void printIn(String msg, Path path) {
 		try {
+			createFile(this.fileName, this.append);
 			PrintWriter printer = new PrintWriter(writer);
 			printer.print(msg);
 			printer.close();
@@ -29,7 +40,6 @@ public class Write {
 		}
 	}
 	
-	
 	public void printIn(String msg) {
 		try {
 			printIn(msg, this.path);
@@ -37,5 +47,29 @@ public class Write {
 			System.out.println(e);
 		}
 	}
+	
+	//read file
+	//if the file is created by the "createFile" you don't need to specify a path
+	
+	public String readFile(Path path) {
+		String txt="";
+		try {
+			FileReader reader = new FileReader(path.toString());
+			BufferedReader bfr = new BufferedReader(reader);
+			String ln = "";
+			while((ln = bfr.readLine()) != null) {
+				txt += ln;
+			}
+		} catch (Exception e) {
+			txt = "Error";
+		}
+		
+		return txt;
+	}
+	
+	public String readFile() {
+		return readFile(this.path);
+	}
+	
 	
 }
