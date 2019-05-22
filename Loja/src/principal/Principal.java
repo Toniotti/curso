23217ff5158242462b32435054ee71148fe27585
Informatica Para Terceira Idade <i3i@proway.treina>
2019-05-22 09:48:02 -0300

    testando

--- Loja/src/principal/Principal.java
+++ Loja/src/principal/Principal.java
@@ -7,19 +7,14 @@ import interf.Login;
 public class Principal {
 	public static void main(String[] args) {
 		Pessoal p = new Pessoal();//instancia a classe "Pessoal"
-		//dale msm irmao
-		//just testing		
+		
 		//seta o primeiro usuario, no caso o administrador
 		p.setNome("admin");
 		p.setPass("admin");
 		Dados.dadosPessoal.add(p);
-		//testando git
+		
 		Login l = new Login();
 		l.setLocationRelativeTo(null);
 		l.setVisible(true);
-		
-		/*
-		 testando a mudança de pasta
-		 */
 	}
 }
