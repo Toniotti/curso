54f4d5e82e42fb549a7baebe9a08bc5db0809020
Informatica Para Terceira Idade <i3i@proway.treina>
2019-05-22 09:24:50 -0300

    testando

--- Loja/src/principal/Principal.java
+++ Loja/src/principal/Principal.java
@@ -7,6 +7,7 @@ import interf.Login;
 public class Principal {
 	public static void main(String[] args) {
 		Pessoal p = new Pessoal();//instancia a classe "Pessoal"
+		//dale msm irmao
 		//just testing		
 		//seta o primeiro usuario, no caso o administrador
 		p.setNome("admin");
