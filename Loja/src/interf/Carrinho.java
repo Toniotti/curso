e3e493ad4f0cddceb7806eb88c0820d19b2cb759
Henrique <henriquepassosarrazo@gmail.com>
2019-05-27 10:39:45 -0300

    1

---
+++ Loja/src/interf/Carrinho.java
@@ -0,0 +1,32 @@
+package interf;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class Carrinho extends JFrame {
+
+	private JPanel contentPane;
+	private JTable tbl2;
+
+	public Carrinho() {
+		setTitle("Carrinho");
+		setBounds(100, 100, 450, 442);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(24, 21, 400, 371);
+		contentPane.add(scrollPane);
+		
+		tbl2 = new JTable();
+		scrollPane.setViewportView(tbl2);
+	}
+}
