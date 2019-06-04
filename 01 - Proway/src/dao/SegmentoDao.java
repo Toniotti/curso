2e654192450a6f7a9f2ea09a366e28e75812f805
Yuri <yurisvaz@hotmail.com>
2019-06-04 11:38:40 -0300

    continuação do banco de dados

--- 01 - Proway/src/dao/SegmentoDao.java
+++ 01 - Proway/src/dao/SegmentoDao.java
@@ -3,148 +3,198 @@ package dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 import beans.Segmento;
 import conexao.Conexao;
 
 public class SegmentoDao {
-	//cadastrar segmento
-	public  boolean cadastrar(String nomeSegmento) {
-		
-		//verificar cadsatro
+	// cadastrar segmento
+	public boolean cadastrar(String nomeSegmento) {
+
+		// verificar cadsatro
 		boolean valida = false;
-		
-		
-		//tentativa de cadastro
+
+		// tentativa de cadastro
 		try {
-			//sql
+			// sql
 			String sql = "INSERT INTO segmentos (nomeSegmento) VALUES (?)";
-			
-			//conexao
+
+			// conexao
 			Conexao c = new Conexao();
-			
-			//executar funcionalidades sql
+
+			// executar funcionalidades sql
 			PreparedStatement pstmt = c.con().prepareStatement(sql);
 			pstmt.setString(1, nomeSegmento);
 			pstmt.execute();
-			
-			//finalizar conexao
+
+			// finalizar conexao
 			c.con().close();
-			
-			//adicionar o valor verdadeira
+
+			// adicionar o valor verdadeira
 			valida = true;
-			
+
 		} catch (Exception e) {
-			System.out.println("Falha no cadastro: "+e.getMessage());
+			System.out.println("Falha no cadastro: " + e.getMessage());
 		}
-		
-		
-		//retorno
+
+		// retorno
 		return valida;
 	}
-	
-	//Selecionar segmento
-	public DefaultComboBoxModel<Segmento> Selecionar(){
-		
-		//instanciar o obj
+
+	// Selecionar segmento
+	public DefaultComboBoxModel<Segmento> Selecionar() {
+
+		// instanciar o obj
 		DefaultComboBoxModel<Segmento> segmento = new DefaultComboBoxModel<Segmento>();
-		
-		//tentar obter os dados
+
+		// tentar obter os dados
 		try {
-			//sql
+			// sql
 			String sql = "SELECT * FROM segmentos";
-			
-			//conexao
+
+			// conexao
 			Conexao c = new Conexao();
-			
-			//executando query e retornando dados
+
+			// executando query e retornando dados
 			Statement stmt = c.con().createStatement();
-			ResultSet rs =  stmt.executeQuery(sql);
-			
-			//finalizar conexao
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// finalizar conexao
 			c.con().close();
-			
-			//percorrer o resultSet
-			while(rs.next()) {
+
+			// percorrer o resultSet
+			while (rs.next()) {
 				Segmento sg = new Segmento(rs.getInt(1), rs.getString(2));
 				segmento.addElement(sg);
 			}
-			
+
 		} catch (Exception e) {
-			System.out.println("Falha ao obter os segmentos: "+e.getMessage());
+			System.out.println("Falha ao obter os segmentos: " + e.getMessage());
 		}
-		
-		//retorno
+
+		// retorno
 		return segmento;
 	}
 
-	//arraylist da tabela
-	private ArrayList<Segmento> arrayTabela(){
-		//arraylist
+	// arraylist da tabela
+	public ArrayList<Segmento> arrayTabela() {
+		// arraylist
 		ArrayList<Segmento> segmento = new ArrayList<Segmento>();
 
 		try {
-			//sql
-		String sql = "SELECT * FROM segmentos";
+			// sql
+			String sql = "SELECT * FROM segmentos";
 
-		//conexao
-		Conexao c = new Conexao();
+			// conexao
+			Conexao c = new Conexao();
 
-		Statement stmt = c.con().createStatement();
+			Statement stmt = c.con().createStatement();
 
-		ResultSet rs = stmt.executeQuery(sql);
+			ResultSet rs = stmt.executeQuery(sql);
 
-		while (rs.next()) {
-			Segmento s = new Segmento(rs.getInt(1), rs.getString(2));
-			segmento.add(s);
-		}
+			while (rs.next()) {
+				Segmento s = new Segmento(rs.getInt(1), rs.getString(2));
+				segmento.add(s);
+			}
 		} catch (Exception e) {
-			System.out.println("Falha ao criar array: "+e.getMessage());
+			System.out.println("Falha ao criar array: " + e.getMessage());
 		}
-
 
 		return segmento;
 	}
 
-	public DefaultTableModel mostrarTabela(){
+	public DefaultTableModel mostrarTabela() {
 		DefaultTableModel dados = new DefaultTableModel();
 		dados.addColumn("Segmento");
 
 		ArrayList<Segmento> segmento = arrayTabela();
-		
-		for(int i=0; i < segmento.size(); i++) {
-			dados.addRow(new Object[] {
-				segmento.get(i).getSegmento(),
-			});
+
+		for (int i = 0; i < segmento.size(); i++) {
+			dados.addRow(new Object[] { segmento.get(i).getSegmento(), });
 		}
 
 		return dados;
 	}
 
-	//alterar tabela
-	public boolean alterarTabela(int linha){
+	// alterar tabela
+	public boolean alterarTabela(String segmento, int linha) throws SQLException {
 
-		//criar variavel de teste
+		// criar variavel de validação
 		boolean valida = false;
+		
+		// conexao
+		Conexao c = new Conexao();
 
 		try {
-			//conexao
-			Conexao c = new Conexao();
+
+			// pegar dados do array
+			ArrayList<Segmento> segmentos = arrayTabela();
+
+			// sql
+			String sql = "UPDATE segmentos SET nomeSegmento = ? WHERE idSegmento = ?";
+			PreparedStatement pstmt = c.con().prepareStatement(sql);
+			pstmt.setString(1, segmento);
+			pstmt.setInt(2, segmentos.get(linha).getId());
+			pstmt.execute();
+
+			// atualiza a validação
+			valida = true;
+		} catch (Exception e) {
+			System.out.println("Erro ao alterar segmento: " + e.getMessage());
+		}finally{
+			c.con().close();
+		}
+
+		// retorno
+		return valida;
+	}
+
+	// deletar segmento
+	public boolean deletar(int linha) throws SQLException {
+		//criar variavel de validação
+		boolean valida = false;
+
+		//conexao
+		Conexao c = new Conexao();
+
+		try {
 			
-			//pegar dados do array
-			ArrayList<Segmento> segmento = arrayTabela();
-			segmento.get(linha).getId();
 
 			//sql
-			String sql = "UPDATE segmentos SET nomeSegmento = ?";
-			PreparedStatement pstmt = 
+			String sql = "DELETE FROM segmentos WHERE idSegmento = ?";
+			String count = "SELECT COUNT(idSegmento) FROM cursos WHERE idSegmento = ?";
+
+			//arrayList
+			ArrayList<Segmento> segmentos = arrayTabela();
+
+			//verifica se o segmento está sendo utilizado em cursos
+			PreparedStatement pstmt = c.con().prepareStatement(count);
+			pstmt.setInt(1, segmentos.get(linha).getId());
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			if(rs.getInt(1) == 0){
+				c.con().close();
+				PreparedStatement pstmt2 = c.con().prepareStatement(sql);
+				pstmt2.setInt(1, segmentos.get(linha).getId());
+				pstmt2.execute();
+	
+				//atualiza variavel de validação
+				valida = true;
+			}else{
+				JOptionPane.showMessageDialog(null, "O segmento está sendo utilizado, portanto não pode ser deletado.");
+			}
+
 		} catch (Exception e) {
-			//TODO: handle exception
+			System.out.println("Erro ao deletar: "+e.getMessage());
+		}finally{
+			c.con().close();
 		}
 
 		//retorno
--- 01 - Proway/src/formulario/Segmento.java
+++ 01 - Proway/src/formulario/Segmento.java
@@ -18,6 +18,8 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 public class Segmento extends JFrame {
@@ -26,6 +28,7 @@ public class Segmento extends JFrame {
 	private JTextField txtSegmento;
 
 	private int linha;
+	private SegmentoDao segmentoDao = new SegmentoDao();
 
 	public Segmento() {
 		setForeground(Color.WHITE);
@@ -35,19 +38,19 @@ public class Segmento extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblSegmento = new JLabel("Informe o nome do segmento:");
 		lblSegmento.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblSegmento.setBounds(10, 23, 199, 22);
 		contentPane.add(lblSegmento);
-		
+
 		txtSegmento = new JTextField();
 		txtSegmento.setBounds(219, 26, 274, 20);
 		contentPane.add(txtSegmento);
 		txtSegmento.setColumns(10);
-		
+
 		JButton btnCadastrar = new JButton("Cadastrar");
-		
+
 		getRootPane().setDefaultButton(btnCadastrar);
 		btnCadastrar.setBounds(10, 87, 120, 30);
 		contentPane.add(btnCadastrar);
@@ -62,39 +65,87 @@ public class Segmento extends JFrame {
 		btnExcluir.setBounds(270, 87, 120, 30);
 		add(btnExcluir);
 
-		//scrol pane
+		// scrol pane
 		JScrollPane scroll = new JScrollPane();
 		scroll.setBounds(10, 120, 525, 235);
 		scroll.setBackground(Color.BLACK);
-		
-		//tabela
+
+		// tabela
 		JTable table = new JTable();
 		SegmentoDao sd = new SegmentoDao();
 		scroll.setViewportView(table);
-		
-		btnAlterar.addActionListener(new ActionListener(){
-			
+
+		btnAlterar.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				String segmento = txtSegmento.getText();// String contendo o novo nome do segmento
+
+				try {
+					// ação de alterar o segmento
+					boolean valida = segmentoDao.alterarTabela(segmento, linha);
+					
+					// mensagem de sucesso ou erro
+					System.out.println(valida == true ? "Segmento alterado com sucesso" : "Erro ao alterar segmento");
+				} catch (Exception e2) {
+					System.out.println("Try botao alterar: "+e2.getMessage());
+				}
+
+				// atualizar tabela
+				table.setModel(sd.mostrarTabela());
+
+				// reseta os botoes
+				btnCadastrar.setEnabled(true);
+				btnAlterar.setEnabled(false);
+				btnExcluir.setEnabled(false);
+
+				// limpar campo e deixar como foco
+				txtSegmento.setText("");
+				txtSegmento.requestFocus();
 			}
 		});
 
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//obeter segmento
+				// obeter segmento
 				String nomeSegmento = txtSegmento.getText();
-				
-				//realizar o cadastro
+
+				// realizar o cadastro
 				SegmentoDao seg = new SegmentoDao();
 				boolean valida = seg.cadastrar(nomeSegmento);
-				
-				//eibir mensagem
+
+				// eibir mensagem
 				JOptionPane.showMessageDialog(null, valida == true ? "Cadastro ok" : "Falha ao cadastrar");
-				
-				//limpar o campo e focus
+
+				// limpar o campo e focus
 				txtSegmento.setText("");
 				txtSegmento.requestFocus();
+
+				//atualiza tabela
+				table.setModel(sd.mostrarTabela());
+			}
+		});
+
+		btnExcluir.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					boolean valida = segmentoDao.deletar(linha);
+				} catch (SQLException e1) {
+					System.out.println("Try botao excluir: "+e1.getMessage());
+				}
+
+				//resetar botoes
+				btnCadastrar.setEnabled(true);
+				btnAlterar.setEnabled(false);
+				btnExcluir.setEnabled(false);
+
+				//limpar e deixar em foco
+				txtSegmento.setText("");
+				txtSegmento.requestFocus();
+
+				//atualiza tabela
 				table.setModel(sd.mostrarTabela());
 			}
 		});
@@ -102,6 +153,13 @@ public class Segmento extends JFrame {
 		table.addMouseListener(new MouseAdapter() {
 			public void mouseReleased(MouseEvent e){
 				linha = table.getSelectedRow();
+
+				ArrayList<beans.Segmento> segmentos = segmentoDao.arrayTabela();
+				txtSegmento.setText(segmentos.get(linha).getSegmento());
+
+				btnCadastrar.setEnabled(false);
+				btnAlterar.setEnabled(true);
+				btnExcluir.setEnabled(true);
 			}
 		});
 
